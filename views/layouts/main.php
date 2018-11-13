050decd6ce37d58ac68195cd3a1863237ed0b05f
aleksei.buldakov <aleksei.buldakov@academmedia.com>
2018-11-13 21:00:16 +0700

    Добавил яндекс метрику

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -100,7 +100,34 @@ AppAsset::register($this);
     <div class="container">
         <p class="pull-left">&copy; Moneyinpocket <?= date('Y') ?></p>
 
-        <p class="pull-right">Здесь будут счетчики</p>
+        <p class="pull-right"><!-- Yandex.Metrika counter -->
+            <script type="text/javascript" >
+                (function (d, w, c) {
+                    (w[c] = w[c] || []).push(function() {
+                        try {
+                            w.yaCounter51124634 = new Ya.Metrika2({
+                                id:51124634,
+                                clickmap:true,
+                                trackLinks:true,
+                                accurateTrackBounce:true
+                            });
+                        } catch(e) { }
+                    });
+
+                    var n = d.getElementsByTagName("script")[0],
+                        s = d.createElement("script"),
+                        f = function () { n.parentNode.insertBefore(s, n); };
+                    s.type = "text/javascript";
+                    s.async = true;
+                    s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+                    if (w.opera == "[object Opera]") {
+                        d.addEventListener("DOMContentLoaded", f, false);
+                    } else { f(); }
+                })(document, window, "yandex_metrika_callbacks2");
+            </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/51124634" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter --></p>
     </div>
 </footer>
 
