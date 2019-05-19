b7a402ef8b18009bbb7f3ebf4fc65e977a4328ea
Aleksey Krazer <vpb1@tpu.ru>
2019-05-19 14:06:32 +0700

    psr с другим автором

--- controllers/AccountingController.php
+++ controllers/AccountingController.php
@@ -30,8 +30,8 @@ class AccountingController extends Controller
                     ],
                 ],
             ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
+            'verbs'  => [
+                'class'   => VerbFilter::className(),
                 'actions' => [
                     'logout' => ['post'],
                 ],
@@ -39,33 +39,19 @@ class AccountingController extends Controller
         ];
     }
 
-    private function yiiSetCookie($name, $value, $expire = 120)
-    {
-        //Записываем куку
-        Yii::$app->response->cookies->add(
-            new Cookie(
-                [
-                    'name' => $name,
-                    'value' => $value,
-                    'expire' => time() + $expire,
-                ]
-            )
-        );
-    }
-
     public function actionHistory($type, $date = false)
     {
-        $date=date("Y-m-d", strtotime($date));
+        $date = date("Y-m-d", strtotime($date));
 
         $operation_action = new OperationAction($type);
-        $type = $operation_action->checkType();
-        $data_for = $operation_action->getDataForDay($date);
+        $type             = $operation_action->checkType();
+        $data_for         = $operation_action->getDataForDay($date);
 
         /*
         Запись куки, чтобы при загрузке в календаре показывалась выбранная для history дата
         */
-        if (date("Y-m-d", strtotime($date))!=date("Y-m-d", time())) {
-            $this->yiiSetCookie("datetime", date("Y-m-d ", strtotime($date)).date("H:i", time()), 120);
+        if (date("Y-m-d", strtotime($date)) != date("Y-m-d", time())) {
+            $this->yiiSetCookie("datetime", date("Y-m-d ", strtotime($date)) . date("H:i", time()), 120);
         } else {
             Yii::$app->response->cookies->remove("datetime");
         }
@@ -73,11 +59,21 @@ class AccountingController extends Controller
         return $this->renderPartial("history", compact('date', 'data_for', 'type'));
     }
 
+    private function yiiSetCookie($name, $value, $expire = 120)
+    {
+        //Записываем куку
+        Yii::$app->response->cookies->add(new Cookie([
+                    'name'   => $name,
+                    'value'  => $value,
+                    'expire' => time() + $expire,
+                ]));
+    }
+
     public function actionNow($type)
     {
         Yii::$app->response->cookies->remove("datetime");
         $operation_action = new OperationAction($type);
-        $type = $operation_action->checkType();
+        $type             = $operation_action->checkType();
 
         return $this->redirect(Url::to(["accounting/index", "type" => $type]));
     }
@@ -87,19 +83,19 @@ class AccountingController extends Controller
 
         $operation_action = new OperationAction(Yii::$app->getRequest()->getQueryParam('type'));
 
-        $type = $operation_action->checkType();
+        $type       = $operation_action->checkType();
         $operations = $operation_action->getOneRow($id);
 
         if (Yii::$app->request->post()) {
             //Валидируем данные и перегоняем в нужный нам формат.
             $operation_validator = new FormProcessing($operations->formName(), Yii::$app->request->post());
-            $add_array = $operation_validator->checkOperationForm();
+            $add_array           = $operation_validator->checkOperationForm();
         }
 
         if (isset($add_array) && $operations->load($add_array)) {
             if ($operations->save()) {
                 Yii::$app->session->setFlash('success', 'Операция успешно записана');
-	            //$this->yiiSetCookie("datetime", date("Y-m-d H:i", strtotime($add_array[$operations->formName()]['datetime'])), 120);
+                //$this->yiiSetCookie("datetime", date("Y-m-d H:i", strtotime($add_array[$operations->formName()]['datetime'])), 120);
 
                 if (isset($add_array[$operations->formName()]['deposit_id'])) {
                     $this->yiiSetCookie("deposit", $add_array[$operations->formName()]['deposit_id'], 3600 * 24);
@@ -107,6 +103,7 @@ class AccountingController extends Controller
                 if (isset($add_array[$operations->formName()]['category_id'])) {
                     $this->yiiSetCookie("category", $add_array[$operations->formName()]['category_id'], 3600 * 24);
                 }
+
                 return $this->redirect(Url::to(["accounting/index", "type" => $type]));
             } else {
                 Yii::$app->session->setFlash('error', 'Error!');
@@ -114,8 +111,8 @@ class AccountingController extends Controller
         }
 
         //Подставляем депозит из куки, чтобы пользователю не надо было каждый раз выбирать его.
-        if ($id === false and isset(Yii::$app->request->cookies['deposit']) and $type!=3) {
-            $operations->deposit_id=Yii::$app->request->cookies['deposit']->value;
+        if ($id === false and isset(Yii::$app->request->cookies['deposit']) and $type != 3) {
+            $operations->deposit_id = Yii::$app->request->cookies['deposit']->value;
         }
         //Подставляем категорию расходов из куки.
         if ($id === false and isset(Yii::$app->request->cookies['category']) and $type != 3) {
@@ -123,17 +120,17 @@ class AccountingController extends Controller
         }
 
         //Решаем, на какую дату нам возвращаться в календаре
-        if ($operations->datetime!='') {
-	        //$this->yiiSetCookie("datetime", date("Y-m-d H:i", strtotime($operations->datetime)), 120);
-            $datetime=$operations->datetime;
-            $date = date("Y-m-d", strtotime($operations->datetime));
+        if ($operations->datetime != '') {
+            //$this->yiiSetCookie("datetime", date("Y-m-d H:i", strtotime($operations->datetime)), 120);
+            $datetime = $operations->datetime;
+            $date     = date("Y-m-d", strtotime($operations->datetime));
         } else {
             if (!isset(Yii::$app->request->cookies['datetime'])) {
-                $date = date("Y-m-d", time());
+                $date     = date("Y-m-d", time());
                 $datetime = date("Y-m-d H:i", time());
             } else {
                 $datetime = Yii::$app->request->cookies['datetime']->value;
-                $date = date("Y-m-d", strtotime(Yii::$app->request->cookies['datetime']->value));
+                $date     = date("Y-m-d", strtotime(Yii::$app->request->cookies['datetime']->value));
             }
         }
 
@@ -148,7 +145,7 @@ class AccountingController extends Controller
     public function actionDelete($id, $type)
     {
         $operation_action = new OperationAction($type);
-        $model = $operation_action->getOneRow($id);
+        $model            = $operation_action->getOneRow($id);
 
         if (empty($model)) {
             Yii::$app->session->setFlash('error', 'Ошибка при удалении #301');
@@ -159,6 +156,7 @@ class AccountingController extends Controller
                 Yii::$app->session->setFlash('error', 'Error!');
             }
         }
+
         return $this->redirect(['accounting/index', 'type' => $type]);
     }
 
