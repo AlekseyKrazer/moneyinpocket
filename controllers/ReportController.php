4a5fff3ddda6828b6582449a84c74289c971e1b1
vpb1 <vpb1@tpu.ru>
2019-05-19 13:37:56 +0700

    psr2

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -5,20 +5,23 @@ namespace app\controllers;
 use app\models\Report;
 use Yii;
 
-class ReportController extends \yii\web\Controller {
+class ReportController extends \yii\web\Controller
+{
     /**
      * @return string
      */
-    public function actionIndex() {
+    public function actionIndex()
+    {
         $model         = new Report();
         $data['data']  = "";
         $data['total'] = "";
 
-        if ( Yii::$app->request->post() ) {
-            $model->load( Yii::$app->request->post() );
-            $data = Report::getDataChart( Yii::$app->request->post()['Report']['datetime'], Yii::$app->request->post()['Report']['type'] );
+        if (Yii::$app->request->post()) {
+            $model->load(Yii::$app->request->post());
+            $data = Report::getDataChart(Yii::$app->request->post()['Report']['datetime'],
+                Yii::$app->request->post()['Report']['type']);
         }
 
-        return $this->render( 'index', compact( "model", "data" ) );
+        return $this->render('index', compact("model", "data"));
     }
 }
