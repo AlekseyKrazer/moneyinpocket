e4f70f4fb2ceacb901d3ca81b8fc8a985e2677c6
vpb1 <vpb1@tpu.ru>
2019-05-19 13:27:40 +0700

    Перевожу код на стандарт PSR
    Добавил JS обработку селектора с выбором периода для автоматического подставления значений.
    
    Убрал выбор операции обмена для вывода истории операций.

--- controllers/ReportController.php
+++ controllers/ReportController.php
@@ -6,18 +6,23 @@ use app\models\Report;
 use Yii;
 
 class ReportController extends \yii\web\Controller {
-	public function actionIndex() {
-		$model         = new Report();
-		$data['data']  = "";
-		$data['total'] = "";
+    /**
+     * @return string
+     */
+    public function actionIndex() {
+        $model         = new Report();
+        $data['data']  = "";
+        $data['total'] = "";
 
-		if ( Yii::$app->request->post() ) {
-			$model->load( Yii::$app->request->post() );
-			$data = Report::getDataChart( Yii::$app->request->post()['Report']['datetime'], Yii::$app->request->post()['Report']['type'] );
-		}
+        if ( Yii::$app->request->post() ) {
+            $model->load( Yii::$app->request->post() );
+            $data = Report::getDataChart( Yii::$app->request->post()['Report']['datetime'], Yii::$app->request->post()['Report']['type'] );
+        }
 
-		return $this->render( 'index', compact( "model", "data" ) );
-		//return $this->render('index2');
-	}
+        return $this->render( 'index', compact( "model", "data" ) );
+    }
 
+    public function actionFlatpickr() {
+        return $this->render( 'index', compact( "model", "data" ) );
+    }
 }
--- views/report/index.php
+++ views/report/index.php
@@ -11,52 +11,134 @@ use yii\widgets\ActiveForm;
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script src="https://code.highcharts.com/modules/export-data.js"></script>
 
+<script>
+    function formatDate(date, format = false) {
+
+        var dd = date.getDate();
+        if (dd < 10) dd = '0' + dd;
+
+        var mm = date.getMonth() + 1;
+        if (mm < 10) mm = '0' + mm;
+
+        var yy = date.getFullYear();
+        if (yy < 10) yy = '0' + yy;
+
+        if (format == "input") {
+            return yy + '-' + mm + '-' + dd;
+        } else {
+            return dd + '-' + mm + '-' + yy;
+        }
+    }
+
+    function startOfWeek(date) {
+        var diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
+
+        return new Date(date.setDate(diff));
+    }
+
+    function endOfWeek(date) {
+        var lastday = date.getDate() - (date.getDay() - 1) + 6;
+        return new Date(date.setDate(lastday));
+
+    }
+
+    function changedate() {
+        let id = document.getElementById("report-daterange").value;
+        let cl = document.getElementById("block");
+        let text = document.getElementById("datetime-info");
+        cl.className = "hide";
+
+        var curr = new Date(); // get current date
+        switch (id) {
+            case "other":
+                cl.className = "show";
+                text.innerHTML = "";
+                break;
+            case "day":
+                firstday = new Date();
+                lastday = new Date();
+                text.innerHTML = "Период: " + formatDate(new Date()) + "<BR><BR>";
+                break;
+            case "week":
+                firstday = startOfWeek(curr);
+                lastday = endOfWeek(curr);
+                break;
+            case "last_7days":
+                var lastday = new Date();
+                var firstday = new Date(curr.setDate(curr.getDate() - curr.getDay() - 6));
+                break;
+            case "month":
+                var lastday = new Date();
+                var firstday = new Date(curr.getFullYear(), curr.getMonth(), 1);
+                break;
+            case "last_month":
+                var firstday = new Date(curr.getFullYear(), curr.getMonth() - 1, 1);
+                var lastday = new Date(curr.getFullYear(), curr.getMonth(), 0);
+                break;
+            case "year":
+                var lastday = new Date();
+                var firstday = new Date(curr.getFullYear(), 0, 1);
+                break;
+            case "last_year":
+                var firstday = new Date(curr.getFullYear() - 1, 0, 1);
+                var lastday = new Date(curr.getFullYear(), 0, 0);
+                break;
+            default:
+                text.innerHTML = "";
+                break;
+        }
+        text.innerHTML = "Период: " + formatDate(firstday) + " - " + formatDate(lastday) + "<BR><BR>";
+        document.getElementById('report-datetime').value = formatDate(firstday, "input") + ' to ' + formatDate(lastday, "input");
+
+    }
+</script>
+<?php
+?>
 <div class="col-md-4">
-    <h1>report/index</h1>
-	<?php $form = ActiveForm::begin( [ 'options' => [ 'style' => 'width: 100%;' ] ] ); ?>
-	<?= $form->field( $model, "type" )->dropDownList( [
-		1 => 'Расходы',
-		2 => 'Доходы',
-		3 => 'Обмен'
-	], [ 'options' => [ 1 => [ 'Selected' => true ] ] ] ) ?>
-	<?= $form->field( $model, "view" )->dropDownList( [
-		0 => 'Детально',
-		1 => 'По категориям'
-	], [ 'options' => [ 1 => [ 'Selected' => true ] ] ] ) ?>
-	<?= $form->field( $model, "daterange" )->dropDownList( [
-		'day'        => 'Этот день',
-		'week'       => 'Эта неделя',
-		'month'      => 'Этот месяц',
-		'year'       => 'Этот год',
-		'last_month' => 'Прошлый месяц',
-		'last_year'  => 'Прошлый год',
-		'other'      => 'Другой период'
-	] ) ?>
-	<?= $form->field( $model, 'datetime' )->widget( Flatpickr::className(), [
-		'locale'  => 'ru',
-		'options' => [
-			'class' => 'form-control',
-		],
+    <h1>Отчеты</h1>
+    <?php $form = ActiveForm::begin( [ 'options' => [ 'style' => 'width: 100%;' ] ] ); ?>
+    <?= $form->field( $model, "type" )->dropDownList( [
+        1 => 'Расходы',
+        2 => 'Доходы'
+    ], [ 'options' => [ 1 => [ 'Selected' => true ] ] ] ) ?>
+    <?= $form->field( $model, "view" )->dropDownList( [
+        0 => 'Детально',
+        1 => 'По категориям'
+    ], [ 'options' => [ 1 => [ 'Selected' => true ] ] ] ) ?>
+    <?= $form->field( $model, "daterange" )->dropDownList( [
+        'day'        => 'Этот день',
+        'week'       => 'Эта неделя',
+        'last_7days' => 'Последние 7 дней',
+        'month'      => 'Этот месяц',
+        'last_month' => 'Прошлый месяц',
+        'year'       => 'Этот год',
+        'last_year'  => 'Прошлый год',
+        'other'      => 'Свой выбор',
+    ], [ 'onchange' => 'changedate()' ] ) ?>
 
-		'clientOptions' => [
-			// config options https://chmln.github.io/flatpickr/options/
-			'mode'        => 'range',
-			'altInput'    => true,
-			'altFormat'   => "j F Y",
-			'defaultDate' => $model->datetime,
-			'dateFormat'  => "Y-m-d",
-			'weekNumbers' => true,
-			'allowInput'  => false,
-
-		],
-	] ) ?>
-	<?= Html::submitButton( 'Составить отчет' ) ?>
-	<?php ActiveForm::end(); ?>
-    <p>
-        You may change the content of this page by modifying
-        the file <code><?= __FILE__; ?></code>.
-    </p>
+    <div id="datetime-info"></div>
+    <div id="block" class="hide">
+        <?= $form->field( $model, 'datetime' )->widget( Flatpickr::className(), [
+            'locale'        => 'ru',
+            'options'       => [
+                'class' => 'form-control',
+            ],
+            'clientOptions' => [
+                // config options https://chmln.github.io/flatpickr/options/
+                'mode'        => 'range',
+                'altInput'    => true,
+                'altFormat'   => "j F Y",
+                'defaultDate' => $model->datetime,
+                'dateFormat'  => "Y-m-d",
+                'weekNumbers' => true,
+                'allowInput'  => false,
+            ]
+        ] ) ?>
+    </div>
+    <?= Html::submitButton( 'Составить отчет' ) ?>
+    <?php ActiveForm::end(); ?>
 </div>
+
 <div class="col-md-4">
     Табличка сводник с графиком
 </div>
@@ -71,7 +153,7 @@ use yii\widgets\ActiveForm;
             type: 'pie'
         },
         title: {
-            text: 'Browser market shares in January, 2018'
+            text: 'Всего потрачено <?= Yii::$app->formatter->asCurrency( $data['total'] ) ?>'
         },
         tooltip: {
             valueDecimals: 2,
